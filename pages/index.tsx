f07202d83127db33aa49a9708673fafb5d4a7cb9
cindy <1177031+cindywu@users.noreply.github.com>
2023-03-13 12:09:48 -0400

    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,10 @@ import Iframe from 'react-iframe'
 export default function Home() {
   return (
     <div className={'h-screen bg-black'}>
-      hello
       <Iframe url="https://elasticbeanstalk-us-east-2-989071256297.s3.us-east-2.amazonaws.com/Gin+Weigh+Wu.mp4"
-        id=""
-        className={'w-screen h-screen'}
-        display="block"
-        position="relative"
+        width="100%"
+        position="absolute"
+        height="100%"
       />
     </div>
   )
